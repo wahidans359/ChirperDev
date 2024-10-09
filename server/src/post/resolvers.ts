8b2c10df49a51821a578d99528de85240ae6dbd2
wahidans359 <111223481+wahidans359@users.noreply.github.com>
2024-10-09 23:36:10 +0530

    aws setup

--- server/src/post/resolvers.ts
+++ server/src/post/resolvers.ts
@@ -1,16 +1,37 @@
 import { Post } from "@prisma/client";
 import { prismaClient } from "../clients/db";
+import {S3Client,PutObjectCommand} from '@aws-sdk/client-s3'
 import { GraphqlContext } from "../interfaces";
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
+
 interface CreatePostPayload {
   content: string;
   imageUrl?: string;
 }
+
+const s3Client = new S3Client({credentials:{
+  accessKeyId:String(process.env.secretkey),
+  secretAccessKey:String(process.env.secretAccessKey)
+}})
 const queries = {
   getAllPosts: async (parent: any, args: any, ctx: GraphqlContext) => {
     const posts = await prismaClient.post.findMany({
         orderBy: { createdAt: "desc" },
     });
-    return posts;
+    getSignedURLForPost: async (parent: any, {imageName,imageType}:{imageType:string,imageName:string}, ctx: GraphqlContext) => {
+      if(!ctx.user || !ctx.user.id)
+        throw new Error("Unauthorized");
+      const alloweImageTypes = ['jpg' , 'jpeg','png','webp'];
+      if(!alloweImageTypes.includes(imageType))
+          throw new Error("Invalid image type");
+
+      const putObjectCommand = new PutObjectCommand({
+        Bucket: "chirper-development",
+        Key:`/uploads/${ctx.user.id}/posts/${imageName}-${Date.now().toString()}.${imageType}`,
+      });
+      const signedUrl = await getSignedUrl(s3Client,putObjectCommand);
+      return signedUrl
+    }
   },
 }
 const mutations = {
